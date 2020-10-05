27a03c4592ec765045260d38c8d720dadcc76b39
fordwind <72396975+fordwind@users.noreply.github.com>
2020-10-05 17:09:18 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 const http = require('http');
-const server = http.createServer((req , res) => res.end('deneme'))
+const server = http.createServer((req , res) => res.end('test'))
 server.listen(process.env.PORT)
