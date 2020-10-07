709c9a4e0bbbae8254b82a6ad5ecf1b8781e6425
fordwind <72396975+fordwind@users.noreply.github.com>
2020-10-07 13:48:33 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 const http = require('http');
 const server = http.createServer((req , res) => res.end('asd'))
 server.listen(process.env.PORT)
+const axios = require('axios')
+
+setInterval(() => axios.get('http://mee6.xyz'))
