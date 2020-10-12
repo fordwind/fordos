96de1e55cccc1dfbfc7ea416a4d419a53d765844
fordwind <72396975+fordwind@users.noreply.github.com>
2020-10-12 15:26:59 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const server = http.createServer((req , res) => res.end('asd'))
 server.listen(process.env.PORT)
 const axios = require('axios')
-
+const WebSocket = require('ws');
 //setInterval(() => axios.get('https://dadlaughbutton.com/increment.php'))
 
 i=0
@@ -22,3 +22,10 @@ async function makePostRequest() {
 
 
 //setInterval(make)
+
+const wss = []
+
+for(let i = 0; i<800; i++){
+    const ws = new WebSocket('wss:/eu-central3312.discord.media',);
+    wss.push(ws)
+}
